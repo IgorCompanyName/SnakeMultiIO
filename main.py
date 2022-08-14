742c20baec619d5c6008eaf20f7fc8d75129e0a4
IgruantyPlay <piuuttri@gmail.com>
2022-08-14 23:10:22 +0200

    Basic controls done

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import pygame as pg
 from snake import Snake
+from colors import *
+from constants import *
 
 pg.init()
-SIZE = WIDTH, HEIGHT = 800, 600
 
-screen = pg.display.set_mode(SIZE)
+screen = pg.display.set_mode(REAL_SIZE)
+internal_screen = pg.Surface(SIZE)
 Running = True
 timer = pg.time.Clock()
 
-snake = Snake((0, 0), 1, 1)
+snake = Snake((int(WIDTH / 2), int(HEIGHT / 2)), (0, 0))
 
 def update():
+    internal_screen.fill(BLACK)
     snake.step()
-    snake.draw(screen)
+    snake.draw(internal_screen)
 
 def main():
+    global Running
     while Running:
         for event in pg.event.get():
-            if event == pg.QUIT:
+            if event.type == pg.QUIT:
                 Running = False
+            if event.type == pg.KEYDOWN or event.type == pg.KEYUP:
+                if event.key == pg.K_LEFT:
+                    snake.change_dir((-1, 0))
+                elif event.key == pg.K_RIGHT:
+                    snake.change_dir((1, 0))
+                elif event.key == pg.K_UP:
+                    snake.change_dir((0, -1))
+                elif event.key == pg.K_DOWN:
+                    snake.change_dir((0, 1))
         update()
+
+        screen.blit(pg.transform.scale(internal_screen, screen.get_rect().size), (0, 0))
         pg.display.update()
-        timer.tick(5)
+
+        timer.tick(6)
 
 if __name__ == "__main__":
     main()
--- snake.py
+++ snake.py
@@ -1,13 +1,29 @@
 import pygame as pg
 from colors import *
+from constants import *
 
 class Snake:
     def __init__(self, start_pos: tuple[int, int], start_dir: tuple[int, int]):
         self.pos = start_pos
-        self.dir = start_pos
+        self.dir = start_dir
     
     def step(self):
-        self.pos += self.dir
+        self.pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
+        if not DIES_ON_BORDER:
+            if self.pos[0] > WIDTH:
+                self.pos = (0, self.pos[1])
+            elif self.pos[0] < 0:
+                self.pos = (WIDTH, self.pos[1])
+            if self.pos[1] > HEIGHT:
+                self.pos = (self.pos[0], 0)
+            elif self.pos[1] < 0:
+                self.pos = (self.pos[0], HEIGHT)
+        else:
+            if 0 > self.pos[0] > WIDTH or 0 > self.pos[1] > HEIGHT:
+                exit(0)
     
+    def change_dir(self, n_dir: tuple[int, int]):
+        self.dir = n_dir
+
     def draw(self, screen: pg.Surface):
-        pg.draw.rect(screen, WHITE, self.pos + (16, 16))
+        screen.set_at(self.pos, WHITE)
